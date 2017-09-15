83caa082176c3851df908f13068b1c9b7f182ac8
Ubuntu <ubuntu@ip-10-10-3-38.sa-east-1.compute.internal>
2017-09-15 14:02:29 +0000

    latest

--- change.sh
+++ change.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-sed -i 's/$DRP_CF_HTTP_PORT/${DRP_CF_HTTP_PORT}/g' /etc/nginx/sites-enabled/webapp.conf
+#sed -i 's/$DRP_CF_HTTP_PORT/${DRP_CF_HTTP_PORT}/g' /etc/nginx/sites-enabled/webapp.conf
+
+export PACT_BROKER_DATABASE_USERNAME=$(echo $DRP_EV_POSTGRESQL_URL | cut -d: -f2 | cut -d/ -f3)
+export PACT_BROKER_DATABASE_PASSWORD=$(echo $DRP_EV_POSTGRESQL_URL | cut -d@ -f1 | cut -d: -f3)
+export PACT_BROKER_DATABASE_HOST=$(echo $DRP_EV_POSTGRESQL_URL | cut -d@ -f2 | cut -d: -f1)
+export PACT_BROKER_DATABASE_NAME=$(echo $DRP_EV_POSTGRESQL_URL | cut -d/ -f4)
+#echo $PACT_BROKER_DATABASE_USERNAME
+#echo $PACT_BROKER_DATABASE_PASSWORD
+#echo $PACT_BROKER_DATABASE_HOST
+#echo $PACT_BROKER_DATABASE_NAME
+
